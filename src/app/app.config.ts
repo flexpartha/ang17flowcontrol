6a450ef05a32e1c06ea9b20d0c719e88b3b16165
flexpartha <partha.chakra2@gmail.com>
2024-12-05 12:59:08 +0530

    update with HashLocationStrategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,16 +5,21 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { environment } from '../environments/environment';
-import { APP_BASE_HREF } from '@angular/common';
+import {
+  APP_BASE_HREF,
+  HashLocationStrategy,
+  LocationStrategy,
+} from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
-    //provideClientHydration(),
+    provideClientHydration(),
     provideHttpClient(withFetch()),
     {
       provide: APP_BASE_HREF,
       useValue: environment.BASE_URL,
     },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
 };
